cdfe743081e864ef966e4227e45e7b18f2c2175b
se668 <akispapapanagiotou@hotmail.com>
2021-01-19 04:48:19 +0200

    Initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,250 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Calculator implements ActionListener {
+
+    JFrame frame;
+    ImageIcon logo;
+    JTextField results;
+    JButton [] numButtons = new JButton[10];
+    JButton [] funcButtons = new JButton[8];
+    Font font;
+    JPanel buttons;
+    GridBagConstraints gbc;
+    double num1=0, num2=0;
+    char operator = ' ';
+
+    Calculator(){
+
+        /* frame */
+        logo = new ImageIcon("logo.jpg");
+        frame = new JFrame("Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setLayout(new BorderLayout());
+        frame.setIconImage(logo.getImage());
+
+        font = new Font("Arial", Font.PLAIN, 30);
+
+        /* results */
+        results = new JTextField();
+        results.setPreferredSize(new Dimension(300, 90));
+        results.setFont(font);
+        results.setBackground(Color.LIGHT_GRAY);
+        results.setBorder(BorderFactory.createEmptyBorder(0 ,8, 0, 0));
+        results.setEditable(false);
+
+        /* buttons */
+        for (int i=0; i<10; i++){
+            numButtons[i] = new JButton(String.valueOf(i));
+            numButtons[i].setFont(font);
+            numButtons[i].setFocusable(false);
+            numButtons[i].setPreferredSize(new Dimension(75, 70));
+            numButtons[i].setMinimumSize(new Dimension(75, 70));
+            numButtons[i].addActionListener(this);
+        }
+
+        funcButtons[0] = new JButton("+");
+        funcButtons[1] = new JButton("-");
+        funcButtons[2] = new JButton("*");
+        funcButtons[3] = new JButton("/");
+        funcButtons[4] = new JButton(".");
+        funcButtons[5] = new JButton("=");
+        funcButtons[6] = new JButton("<-");
+        funcButtons[7] = new JButton("C");
+
+        for(int i=0; i<8; i++){
+            funcButtons[i].setFont(font);
+            funcButtons[i].setFocusable(false);
+            funcButtons[i].setPreferredSize(new Dimension(75, 70));
+            funcButtons[i].setMinimumSize(new Dimension(75, 70));
+            funcButtons[i].addActionListener(this);
+        }
+
+        /* panel with buttons */
+        buttons = new JPanel(new GridBagLayout());
+        gbc = new GridBagConstraints();
+
+        /* 1) Row */
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        buttons.add(funcButtons[7], gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        buttons.add(funcButtons[6], gbc);
+
+        gbc.gridx = 2;
+        gbc.gridy = 0;
+        buttons.add(funcButtons[3], gbc);
+
+        gbc.gridx = 3;
+        gbc.gridy = 0;
+        buttons.add(funcButtons[2], gbc);
+
+        /* 2) Row */
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        buttons.add(numButtons[7], gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        buttons.add(numButtons[8], gbc);
+
+        gbc.gridx = 2;
+        gbc.gridy = 1;
+        buttons.add(numButtons[9], gbc);
+
+        gbc.gridx = 3;
+        gbc.gridy = 1;
+        buttons.add(funcButtons[1], gbc);
+
+        /* 3) Row */
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        buttons.add(numButtons[4], gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 2;
+        buttons.add(numButtons[5], gbc);
+
+        gbc.gridx = 2;
+        gbc.gridy = 2;
+        buttons.add(numButtons[6], gbc);
+
+        gbc.gridx = 3;
+        gbc.gridy = 2;
+        buttons.add(funcButtons[0], gbc);
+
+        /* 4) Row */
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        buttons.add(numButtons[1], gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 3;
+        buttons.add(numButtons[2], gbc);
+
+        gbc.gridx = 2;
+        gbc.gridy = 3;
+        buttons.add(numButtons[3], gbc);
+
+        gbc.gridx = 3;
+        gbc.gridy = 3;
+        gbc.gridheight=2;
+        gbc.fill = GridBagConstraints.VERTICAL;
+        buttons.add(funcButtons[5], gbc);
+
+        /* 5) Row */
+        gbc.gridx = 0;
+        gbc.gridy = 4;
+        gbc.gridwidth=2;
+        gbc.gridheight=1;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        buttons.add(numButtons[0], gbc);
+
+        gbc.gridx = 2;
+        gbc.gridy = 4;
+        gbc.gridwidth=1;
+        gbc.gridheight=1;
+        buttons.add(funcButtons[4], gbc);
+
+        /* adds the components to the frame */
+        frame.add(results, BorderLayout.NORTH);
+        frame.add(buttons, BorderLayout.CENTER);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    public String getResults(){
+        return results.getText();
+    }
+
+    public void setResults(String string){
+        results.setText(string);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        /* input numbers */
+        for(int i=0; i<10; i++)
+            if(e.getSource() == numButtons[i] && !getResults().equals("0"))
+                setResults(getResults() + i);
+
+        /* functions */
+        if(getResults().length() > 0){
+            /* (+) */
+            if(e.getSource() == funcButtons[0]){
+                num1 = Double.parseDouble(getResults());
+                operator = '+';
+                setResults("");
+            }
+
+            /* (-) */
+            if(e.getSource() == funcButtons[1]){
+                num1 = Double.parseDouble(getResults());
+                operator = '-';
+                setResults("");
+            }
+
+            /* (*) */
+            if(e.getSource() == funcButtons[2]){
+                num1 = Double.parseDouble(getResults());
+                operator = '*';
+                setResults("");
+            }
+
+            /* (/) */
+            if(e.getSource() == funcButtons[3]){
+                num1 = Double.parseDouble(getResults());
+                operator = '/';
+                setResults("");
+            }
+
+            /* (=) */
+            if(operator != ' '){
+                if(e.getSource() == funcButtons[5]){
+                    num2 = Double.parseDouble(getResults());
+                    switch(operator){
+                        case '+':
+                            num2 = num1 + num2;
+                            operator = ' ';
+                            break;
+                        case '-':
+                            num2 = num1 - num2;
+                            operator = ' ';
+                            break;
+                        case '*':
+                            num2 = num1 * num2;
+                            operator = ' ';
+                            break;
+                        case '/':
+                            num2 = num1 / num2;
+                            operator = ' ';
+                            break;
+                    }
+                    setResults(String.valueOf(num2));
+                }
+            }
+
+            /* (.) */
+            if(e.getSource() == funcButtons[4] && !getResults().contains("."))
+                setResults(getResults() + '.');
+
+            /* (<-) */
+            if(e.getSource() == funcButtons[6])
+                setResults(getResults().substring(0, getResults().length() - 1));
+        }
+
+        /* (C) function */
+        if(e.getSource() == funcButtons[7])
+            setResults("");
+    }
+
+    public static void main(String[] args){
+        new Calculator();
+    }
+}
